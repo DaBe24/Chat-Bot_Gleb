c9b9c23d2f80282dc01cac5ee29b6e69c13bd391
DaBe24 <71638875+DaBe24@users.noreply.github.com>
2020-10-06 11:23:50 +0500

    ToStart Bot

--- src/com/company/Bot.java
+++ src/com/company/Bot.java
@@ -1,14 +1,74 @@
 package com.company;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Bot {
-    public String upMsg() {
+
+    public static HashMap<String, String> Status = new HashMap<>();
+
+    public String readMessage() {
         Scanner in = new Scanner(System.in);
         return in.nextLine();
     }
 
-    public void sendMsg(String msg) {
-        System.out.println(msg);
+    public void sendMessage(String text) {
+        System.out.println(text);
+    }
+
+    public String reply(String text, String name) {
+        if ("wait".equals(Status.get(name))) {
+            Status.put(name, text);
+        }
+        switch (Status.get(name)) {
+            case "/help" -> {
+                Status.put(name, "wait");
+                return DoHelp();
+            }
+            case "start2" -> {
+                sendMessage(text);
+                Status.put(text, "wait");
+                return "Nice to meet you, " + text + "!" +
+                        "\nType '/help' to see my commands.";
+            }
+            case "/reset" -> {
+                Status.put(name, "start2");
+                return DoReset();
+            }
+            case "/play" -> {
+                Status.put(name, "wait");
+                return DoPlay();
+            }
+            case "/dead" -> {
+                Status.put(name, "wait");
+                return DoDead(name);
+            }
+            default -> {
+                Status.put(name, "wait");
+                return "I don't understand!";
+            }
+        }
+    }
+
+    private static String DoReset() {
+        return "What is your new name?";
+    }
+
+    private static String DoDead(String name) {
+        Status.clear();
+        return name + ", goodbye. Gleb is out!";
+    }
+
+    private static String DoPlay() {
+        return "null";
+    }
+
+    private static String DoHelp() {
+        return "Oh hello there! I am Gleb, your text-chat bot." +
+                "\nMy command list is:" +
+                "\n/help - command list;" +
+                "\n/dead - end current session;" +
+                "\n/play - doesn't work" +
+                "\n/reset - rename";
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,52 +1,27 @@
 package com.company;
 
-import java.util.HashMap;
-
 public class Main
 {
-    public static HashMap <String, String>Status = new HashMap<>();
-
     public static void main(String[] args) {
-        CreateBot();
-        /*Bot gleb = new Bot();
-        gleb.sendMsg("Write \"/start\" to start work");
-        while (true){
-            String text = gleb.upMsg();
-            switch (text) {
-                case "/help" -> gleb.sendMsg("Oh hello there! I am Gleb, your text-chat bot." +
-                        "\nMy command list is:" +
-                        "\n/help - command list;" +
-                        "\n/dead - end current session;" +
-                        "\n/play - doesn't work");
-                case "/start" -> {
-                    gleb.sendMsg("I am Gleb. What is your name?");
-                    String name = gleb.upMsg();
-                    Status = "Waite";
-                    gleb.sendMsg("Nice to meet you, " + name + "!" +
-                            "\nType '/help' to see my commands.");
-                }
-                case "/play" -> {
-                    gleb.sendMsg("null");
-                }
-                case "/dead" -> {
-                    gleb.sendMsg("I died!!!");
-                    return;
-                }
-                default -> gleb.sendMsg("I don't understand!");
-            }
-        }*/
-    }
-
-    private static void CreateBot() {
         Bot gleb = new Bot();
-        gleb.sendMsg("I am Gleb. What is your name?");
-        String name = gleb.upMsg();
-        Status.put(name, "/start");
-        gleb.sendMsg("Nice to meet you, " + name + "!" +
-                "\nType '/help' to see my commands.");
-        UpMsg(gleb, name);
+        gleb.sendMessage("I am Gleb. What is your name? " +
+                "\n(Write your name to start work)");
+        String name = "user";
+        while (true){
+            String text = gleb.readMessage();
+            if (Bot.Status.isEmpty() || Bot.Status.get(name).equals("start2")) {
+                Bot.Status.put(text, "start2");
+                name = text;
+            }
+            String result = gleb.reply(text, name);
+            gleb.sendMessage(result);
+            if (Bot.Status.isEmpty())
+                return;
+        }
     }
+}
 
+/*
     private static void CommandList(Bot gleb, String name){
         switch (Status.get(name)) {
             case "/help" -> DoHelp(gleb, name);
@@ -59,37 +34,6 @@ public class Main
             }
         }
     }
+*/
 
-    private static void UpMsg(Bot gleb, String name) {
-        Status.put(name, gleb.upMsg());
-        CommandList(gleb, name);
-    }
-
-    private static void DoReset(Bot gleb) {
-        gleb.sendMsg("What is your new name?");
-        String name = gleb.upMsg();
-        gleb.sendMsg("Nice to meet you, " + name + "!");
-        UpMsg(gleb, name);
-    }
-
-    private static void DoDead(Bot gleb, String name) {
-        gleb.sendMsg(name + ", goodbye. Gleb is out!");
-        Status.clear();
-    }
-
-    private static void DoPlay(Bot gleb, String name) {
-        gleb.sendMsg("null");
-        UpMsg(gleb, name);
-    }
-
-    private static void DoHelp(Bot gleb, String name) {
-        gleb.sendMsg("Oh hello there! I am Gleb, your text-chat bot." +
-                "\nMy command list is:" +
-                "\n/help - command list;" +
-                "\n/dead - end current session;" +
-                "\n/play - doesn't work" +
-                "\n/reset - rename");
-        UpMsg(gleb, name);
-    }
-}
 
